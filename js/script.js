a83a46215e1a01f7f15a9afcc743519b7b2d5210
lailaamr2 <130404835+lailaamr2@users.noreply.github.com>
2023-08-19 05:06:18 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var btn_copy = document.getElementById("btn_copy")
+var btn_copy2 = document.getElementById("btn_copy2")
+var btn_copy3= document.getElementById("btn_copy3")
+btn_copy.onclick= function(){
+    alert("copied")
+}
+btn_copy2.onclick= function(){
+    alert("copied")
+}
+btn_copy3.onclick= function(){
+    alert("copied")
+}
